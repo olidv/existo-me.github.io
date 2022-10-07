64cd4335c0d383b0d92d56bcaef70b96cfbb2001
Raul Bras <br.eve.author@gmail.com>
2022-10-06 23:10:38 -0300

    Implementado layout inicial.

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,11 @@
 (function ($) {
     ("use strict");
 
-    // Show toast with the carousel navigation instructions:
-    const navigationToast = document.getElementById("navigationToast");
-    const nvToast = new bootstrap.Toast(navigationToast);
-    //nvToast.show();
+
+
+
+
+
+
 
 })(jQuery);
