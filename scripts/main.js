589598427afb59637d0467f762000d7f21400a05
Raul Bras <br.eve.author@gmail.com>
2022-10-07 23:25:23 -0300

    Layout dos formularios pronto.

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,16 @@
+const myCarouselElement = document.querySelector("#carouselTest");
+const carousel = new bootstrap.Carousel(myCarouselElement, {
+    interval: false,
+    keyboard: false,
+    pause: true,
+    wrap: false,
+});
+
+
 (function ($) {
     ("use strict");
 
-
-
-
-
-
-
+    carousel.pause();
+    carousel.to(2);
 
 })(jQuery);
