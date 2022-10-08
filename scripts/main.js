afb595e99e64b14fe24662718d8efd2f84f4f542
Raul Bras <br.eve.author@gmail.com>
2022-10-08 11:38:54 -0300

    Implementacao do form slider.

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,18 @@
 const myCarouselElement = document.querySelector("#carouselTest");
 const carousel = new bootstrap.Carousel(myCarouselElement, {
-    interval: false,
-    keyboard: false,
-    pause: true,
-    wrap: false,
+    // interval: false,
+    // keyboard: false,
+    // pause: true,
+    // wrap: false,
+    interval: 60000,
+    wrap: true,
 });
 
 
 (function ($) {
     ("use strict");
 
-    carousel.pause();
-    carousel.to(2);
+    // carousel.to(1);
+    // carousel.pause();
 
 })(jQuery);
